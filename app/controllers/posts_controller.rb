0dc1cac62e8c814612af43c7634074ad4fff710e
condraft <condraft64@gmail.com>
2019-10-04 21:35:15 +0900

    TempSave

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,30 @@
 class PostsController < ApplicationController
-  def index    
+
+  before_action :move_to_index, except: [:index, :show]
+
+  def index
+    @post = Post.includes(:user).page(params[:page]).per(5).order("created_at DESC")
   end
+  
+  def new
+  end
+  
+  def create
+    Post.create(image: post_params[:image], text: post_params[:text], user)
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy if post.user_id == current_user.id
+  end
+
+  private
+  def post_params
+    params.permit(:image, :text)
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
+  end
+
 end
